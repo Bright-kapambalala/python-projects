0b408fa76e77add2c0a94fa5b86ad4b943151150
Bright-kapambalala <kapambalalabright@gmail.com>
2025-09-19 02:33:34 +0200

    Add user_profile script

---
+++ user_profile.py
@@ -0,0 +1,6 @@
+
+name = str(input("what is your name: "))
+age = int(input("how old are you?: "))
+favlanguage = str(input("what is your favourite programming language?: "))
+hobby = str(input("what are your hobbies?: "))
+print ("hello", name,"!","\nYou are", age,"yo.", "\nyour favourite programming language is",favlanguage, "and your hobby is", hobby)
